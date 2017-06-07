9b9daff262b69cb918831e2e823634b9c43ee590
Caio Marchi <caiomga@gmail.com>
2017-06-07 19:47:30 -0300

    Line Clear Bug FIxed

--- js/gamelogic.js
+++ js/gamelogic.js
@@ -1,13 +1,39 @@
 var game = new Phaser.Game(640, 640, Phaser.AUTO, '', {preload: preload, create: create, update: update});
+
 var BLOCK_SIDE = 32;
 var MAX_BLOCK_COUNT_HORIZONTAL = 10;
 var MAX_BLOCK_COUNT_VERTICAL = 20;
 var MAX_INDEX_HORIZONTAL = 9;
 var MAX_INDEX_VERTICAL = 20;
 var VERTICAL_OFFSET = 1;
-var blocos = [];
+
 var tetraminos;
+var blocos = [];
 var board = [
+	['_','_','_','_','_','_','_','_','_','_'],//DEADZONE if drop occurs here it means GAMEOVER
+	['_','_','_','_','_','_','_','_','_','_'],
+	['_','_','_','_','_','_','_','_','_','_'],
+	['_','_','_','_','_','_','_','_','_','_'],
+	['_','_','_','_','_','_','_','_','_','_'],
+	['_','_','_','_','_','_','_','_','_','_'],
+	['_','_','_','_','_','_','_','_','_','_'],
+	['_','_','_','_','_','_','_','_','_','_'],
+	['_','_','_','_','_','_','_','_','_','_'],
+	['_','_','_','_','_','_','_','_','_','_'],
+	['_','_','_','_','_','_','_','_','_','_'],
+	['_','_','_','_','_','_','_','_','_','_'],
+	['_','_','_','_','_','_','_','_','_','_'],
+	['_','_','_','_','_','_','_','_','_','_'],
+	['_','_','_','_','_','_','_','_','_','_'],
+	['_','_','_','_','_','_','_','_','_','_'],
+	['_','_','_','_','_','_','_','_','_','_'],
+	['_','X','X','X','X','X','X','X','X','X'],
+	['_','X','X','X','X','X','X','X','X','X'],
+	['_','X','X','X','X','X','X','X','X','X'],
+	['X','X','X','X','X','X','X','X','X','_']
+	];
+/*
+	var board = [
 	['_','_','_','_','_','_','_','_','_','_'],//DEADZONE if drop occurs here it means GAMEOVER
 	['_','_','_','_','_','_','_','_','_','_'],
 	['_','_','_','_','_','_','_','_','_','_'],
@@ -30,6 +56,7 @@ var board = [
 	['_','_','_','_','_','_','_','_','_','_'],
 	['_','_','_','_','_','_','_','_','_','_']
 	];
+	*/
 var boardDisplay = [
 	[,,,,,,,,,],
 	[,,,,,,,,,],
@@ -56,9 +83,7 @@ var nextWindow = [
 	[,,],
 	[,,],
 	[,,],
-	[,,]
-];
-
+	[,,] ];
 var curX = 0;
 var curY = 0;
 var curPose = 0;
@@ -69,12 +94,13 @@ var movementLock = false;
 var movementInterval = 0.15;
 var hAxis = 0;
 var timer = null;
+
 var gameover = false;
 
 //===========================TODO==================================:
 // FIX TIMER ISSUES - DONE
 // SHOW NEXT PIECE - DONE
-// GAME OVER - 
+// GAME OVER - DONE
 // LINE CLEAR ANIMATION
 // GAME OVER ANIMATION
 // COLORS
@@ -104,6 +130,7 @@ function create(){
 	tetraminos = game.cache.getJSON('tetraminosJSON');
 	getPiece();
 	getPiece();//proper init
+	testTick();
 
 }
 
@@ -119,18 +146,44 @@ function update(){
 	}
 }
 
-function getPiece(){
-	piece = nextPiece;
-	nextPiece = tetraminos.tetraminos[game.rnd.integerInRange(0, 6)];
-	curPose = 0;
+function blocoOff(x, y){ 
+	boardDisplay[y][x].frameName = 'OFF';
 }
 
 function blocoOn(x, y){ //lits bloco at positio x, y
 	boardDisplay[y][x].frameName = 'ON';
 }
 
-function blocoOff(x, y){ 
-	boardDisplay[y][x].frameName = 'OFF';
+function clearBoardDisplay(){
+	for(var i = 0; i < MAX_BLOCK_COUNT_HORIZONTAL; i++){
+		for(var j = 0; j < MAX_BLOCK_COUNT_VERTICAL; j++){
+			blocoOff(i, j);
+		}
+	}
+}
+
+function clearNextWindow(){
+	for(var i = 0; i < 3; i++){
+		for(var j = 0; j < 4; j++){
+			nextWindow[j][i].frameName = "OFF";
+		}
+	}
+}
+
+function clearPiece(){
+	var tmpX;
+	var tmpY;
+	for(var i = 0; i < 4; i++){
+		tmpX = piece.poses[curPose][i][0] + curX ;
+		tmpY = piece.poses[curPose][i][1] + curY;
+		if(tmpX < 0 || tmpY < 0){
+			// do nothing
+		} else if(tmpX > MAX_INDEX_HORIZONTAL || tmpY > MAX_INDEX_VERTICAL){
+			// do nothing
+		} else {
+			board[tmpY][tmpX] = "_";
+		}
+	}
 }
 
 function createBoardDisplay(){
@@ -146,81 +199,6 @@ function createNextWindow(){
 	for(var i = 0; i < 3; i++){
 		for(var j = 0; j < 4; j++){
 			nextWindow[j][i] = game.add.sprite((i +11) * BLOCK_SIDE , (j +1) * BLOCK_SIDE, 'blocoatlas', 'OFF');
-		}
-	}
-}
-
-function updateBoardDisplayed(){
-	for(var i = 0; i < MAX_BLOCK_COUNT_HORIZONTAL; i++){
-		for(var j = 0; j < MAX_BLOCK_COUNT_VERTICAL; j++){
-			if(board[j+VERTICAL_OFFSET][i] == "_"){
-				blocoOff(i, j);
-			} else {
-				blocoOn(i, j);
-			}
-		}
-	}
-}
-
-function updateNextWindow(){
-	var offsetX = 0;
-	var offsetY = 3;
-	clearNextWindow();
-	for(var i = 0; i < 4; i++){
-		var blocoX = (nextPiece.poses[0][i][0]) + offsetX;
-		var blocoY = (nextPiece.poses[0][i][1]) + offsetY;
-		nextWindow[blocoY][blocoX].frameName = "ON";
-	}
-}
-
-function clearNextWindow(){
-	for(var i = 0; i < 3; i++){
-		for(var j = 0; j < 4; j++){
-			nextWindow[j][i].frameName = "OFF";
-		}
-	}
-}
-
-function clearBoardDisplay(){
-	for(var i = 0; i < MAX_BLOCK_COUNT_HORIZONTAL; i++){
-		for(var j = 0; j < MAX_BLOCK_COUNT_VERTICAL; j++){
-			blocoOff(i, j);
-		}
-	}
-}
-
-function tick(){ //move piece a line down
-	clearPiece();
-	curY++;
-	drawPiece();
-}
-
-function testTick(){
-	if(testDrop()){
-		tick();
-	} else {
-		if(!testGameOver()){
-			newPiece();
-			testLineClear();
-			drawPiece();
-		}
-
-	}
-	setTimeout(testTick, 100);
-}
-
-function clearPiece(){
-	var tmpX;
-	var tmpY;
-	for(var i = 0; i < 4; i++){
-		tmpX = piece.poses[curPose][i][0] + curX ;
-		tmpY = piece.poses[curPose][i][1] + curY;
-		if(tmpX < 0 || tmpY < 0){
-			// do nothing
-		} else if(tmpX > MAX_INDEX_HORIZONTAL || tmpY > MAX_INDEX_VERTICAL){
-			// do nothing
-		} else {
-			board[tmpY][tmpX] = "_";
 		}
 	}
 }
@@ -241,6 +219,88 @@ function drawPiece(){
 	}
 }
 
+function getInput(){
+	hAxis = 0;
+	if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
+		hAxis --;
+	} else if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+		hAxis++;
+	} else {
+		unlockMovement();
+	}
+
+	if(hAxis > 0){
+		if(!movementLock){
+			moveRight();
+		}
+	} else if(hAxis < 0){
+		if(!movementLock){
+			moveLeft();
+		}
+	} else {
+		unlockMovement();
+	}
+	
+	if(game.input.keyboard.isDown(Phaser.Keyboard.UP)){
+		if(!rotateLock){
+			rotateClockWise();
+			rotateLock = true;
+		}
+	} else {
+		rotateLock = false;
+	}
+}
+
+function getPiece(){
+	piece = nextPiece;
+	nextPiece = tetraminos.tetraminos[game.rnd.integerInRange(0, 6)];
+	curPose = 0;
+}
+
+function lineClear(lineNum){
+	var prevLine;
+	for(var i = lineNum; i > 0; i--){
+		prevLine = i -1;
+		for(var j=0; j< MAX_BLOCK_COUNT_HORIZONTAL; j++){
+			board[i][j] = board[prevLine][j];
+		}
+	}
+	
+	for(var i=0; i< MAX_BLOCK_COUNT_HORIZONTAL; i++){
+			board[0][i] = "_";
+	}
+	console.log("Line "+lineNum+" cleared");
+}
+
+function moveRight(){
+	if(testMoveRight()){
+		clearPiece();
+		curX ++;
+		drawPiece();
+	}
+	unlockMovement();
+	movementLock = true;
+	timer = game.time.events.loop(Phaser.Timer.SECOND * movementInterval, unlockMovement, this);
+}
+
+function moveLeft(){
+	if(testMoveLeft()){
+		clearPiece();
+		curX --;
+		drawPiece();
+	}	
+	unlockMovement();
+	movementLock = true;
+	timer = game.time.events.add(Phaser.Timer.SECOND * movementInterval, unlockMovement, this);
+}
+
+function newPiece(){
+	placeOnBoard();
+	getPiece();
+	curY = -2;
+	curX = 4;
+}
+
 function placeOnBoard(){
 	var tmpX;
 	var tmpY;
@@ -254,6 +314,45 @@ function placeOnBoard(){
 		} else {
 			board[tmpY][tmpX] = "X";
 		}
+	}
+}
+
+function printBoard(showOffset = true){
+	var tmpOffset;
+	if(showOffset){
+		tmpOffset = VERTICAL_OFFSET;
+	} else {
+		tmpOffset = 0;
+	}
+	var line = "";
+	for (var i = tmpOffset; i <= MAX_BLOCK_COUNT_VERTICAL; i++){
+		line = i + " - ";
+		for (var j = 0; j < MAX_BLOCK_COUNT_HORIZONTAL; j++){
+			line+= board[i][j];
+		}
+		console.log(line);
+	}
+}
+
+function rotateClockWise(){
+	if(testRotateClockWise()){
+		clearPiece();
+		curPose++;
+		if(curPose > 3){
+			curPose = 0;
+		}
+		drawPiece();
+	} 
+}
+
+function rotateCounterClockWise(){
+	if(testRotateCounterClockWise()){
+		clearPiece();
+		curPose--;
+		if(curPose < 0){
+			curPose = 3;
+		}
+		drawPiece();
 	}
 }
 
@@ -279,25 +378,28 @@ function testDrop(){
 	return true;
 }
 
-function testMoveRight(){
-	var tmpX;
-	var tmpY;
-	for(var i = 0; i < 4; i++){
-			tmpX = piece.poses[curPose][i][0] + curX + 1;
-			tmpY = piece.poses[curPose][i][1] + curY;
-		if(tmpX <= MAX_INDEX_HORIZONTAL){ // test if there is room to go right
-			if(tmpY < 0){ //if offscreen
-				//do nothing
-			} else if(board[tmpY][tmpX] != "X"){
-				//do nothing
-			} else {
-				return false;
+function testGameOver(){
+	if(curY == 0){
+		gameover = true;
+		return true;
+	}
+	return false;
+}
+
+function testLineClear(){
+	for(var i=0; i <= MAX_BLOCK_COUNT_VERTICAL; i++){
+		for(var j=0; j < MAX_BLOCK_COUNT_HORIZONTAL; j++){
+			lineCleared = true;
+			if(board[i][j] == "_"){
+				lineCleared = false;
+				break;
 			}
-		} else {
-			return false;
+		}
+		if(lineCleared){
+			lineClear(i);
+			i--;
 		}
 	}
-	return true;
 }
 
 function testMoveLeft(){
@@ -321,58 +423,25 @@ function testMoveLeft(){
 	return true;
 }
 
-function moveRight(){
-	if(testMoveRight()){
-		clearPiece();
-		curX ++;
-		drawPiece();
-	}
-	unlockMovement();
-	movementLock = true;
-	timer = game.time.events.loop(Phaser.Timer.SECOND * movementInterval, unlockMovement, this);
-}
-
-function moveLeft(){
-	if(testMoveLeft()){
-		clearPiece();
-		curX --;
-		drawPiece();
-	}	
-	unlockMovement();
-	movementLock = true;
-	timer = game.time.events.add(Phaser.Timer.SECOND * movementInterval, unlockMovement, this);
-}
-
-function testGameOver(){
-	if(curY == 0){
-		gameover = true;
-		return true;
-	}
-	return false;
-}
-
-function newPiece(){
-	placeOnBoard();
-	getPiece();
-	curY = -2;
-	curX = 4;
-}
-
-function printBoard(showOffset = true){
-	var tmpOffset;
-	if(showOffset){
-		tmpOffset = VERTICAL_OFFSET;
-	} else {
-		tmpOffset = 0;
-	}
-	var line = "";
-	for (var i = 0; i < MAX_BLOCK_COUNT_VERTICAL; i++){
-		line = i + " - ";
-		for (var j = 0; j < MAX_BLOCK_COUNT_HORIZONTAL; j++){
-			line+= board[i + tmpOffset][j];
+function testMoveRight(){
+	var tmpX;
+	var tmpY;
+	for(var i = 0; i < 4; i++){
+			tmpX = piece.poses[curPose][i][0] + curX + 1;
+			tmpY = piece.poses[curPose][i][1] + curY;
+		if(tmpX <= MAX_INDEX_HORIZONTAL){ // test if there is room to go right
+			if(tmpY < 0){ //if offscreen
+				//do nothing
+			} else if(board[tmpY][tmpX] != "X"){
+				//do nothing
+			} else {
+				return false;
+			}
+		} else {
+			return false;
 		}
-		console.log(line);
 	}
+	return true;
 }
 
 function testRotateClockWise(){
@@ -422,58 +491,24 @@ function testRotateCounterClockWise(){
 	return true;
 }
 
-function rotateClockWise(){
-	if(testRotateClockWise()){
-		clearPiece();
-		curPose++;
-		if(curPose > 3){
-			curPose = 0;
+function testTick(){
+	if(testDrop()){
+		tick();
+	} else {
+		if(!testGameOver()){
+			newPiece();
+			testLineClear();
+			drawPiece();
 		}
-		drawPiece();
-	} 
+
+	}
+	setTimeout(testTick, 100);
 }
 
-function rotateCounterClockWise(){
-	if(testRotateCounterClockWise()){
-		clearPiece();
-		curPose--;
-		if(curPose < 0){
-			curPose = 3;
-		}
-		drawPiece();
-	}
-}
-
-function getInput(){
-	hAxis = 0;
-	if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
-		hAxis --;
-	} else if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
-		hAxis++;
-	} else {
-		unlockMovement();
-	}
-
-	if(hAxis > 0){
-		if(!movementLock){
-			moveRight();
-		}
-	} else if(hAxis < 0){
-		if(!movementLock){
-			moveLeft();
-		}
-	} else {
-		unlockMovement();
-	}
-	
-	if(game.input.keyboard.isDown(Phaser.Keyboard.UP)){
-		if(!rotateLock){
-			rotateClockWise();
-			rotateLock = true;
-		}
-	} else {
-		rotateLock = false;
-	}
+function tick(){ //move piece a line down
+	clearPiece();
+	curY++;
+	drawPiece();
 }
 
 function unlockMovement(){
@@ -481,36 +516,27 @@ function unlockMovement(){
 	if(timer != null){
 		game.time.events.remove(timer);
 	}
-	
 }
 
-function testLineClear(){
-	for(var i=0; i < MAX_BLOCK_COUNT_VERTICAL; i++){
-		for(var j=0; j < MAX_BLOCK_COUNT_HORIZONTAL; j++){
-			lineCleared = true;
-			if(board[i][j] == "_"){
-				lineCleared = false;
-				break;
+function updateBoardDisplayed(){
+	for(var i = 0; i < MAX_BLOCK_COUNT_HORIZONTAL; i++){
+		for(var j = 0; j < MAX_BLOCK_COUNT_VERTICAL; j++){
+			if(board[j+VERTICAL_OFFSET][i] == "_"){
+				blocoOff(i, j);
+			} else {
+				blocoOn(i, j);
 			}
 		}
-		if(lineCleared){
-			lineClear(i);
-			i--;
-		}
 	}
 }
 
-function lineClear(lineNum){
-	var prevLine;
-	for(var i = lineNum; i > 0; i--){
-		prevLine = i -1;
-		for(var j=0; j< MAX_BLOCK_COUNT_HORIZONTAL; j++){
-			board[i][j] = board[prevLine][j];
-		}
+function updateNextWindow(){
+	var offsetX = 0;
+	var offsetY = 3;
+	clearNextWindow();
+	for(var i = 0; i < 4; i++){
+		var blocoX = (nextPiece.poses[0][i][0]) + offsetX;
+		var blocoY = (nextPiece.poses[0][i][1]) + offsetY;
+		nextWindow[blocoY][blocoX].frameName = "ON";
 	}
-	
-	for(var i=0; i< MAX_BLOCK_COUNT_HORIZONTAL; i++){
-			board[0][i] = "_";
-	}
-	console.log("Line "+lineNum+" cleared");
 }
