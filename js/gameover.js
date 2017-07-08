e135dfc73affbd1433dbc7fb7ad54bcb6c757534
Caio Marchi <caiomga@gmail.com>
2017-07-07 22:16:45 -0300

    Gameover screen (FINALLY) redone. Texts updated.

--- js/gameover.js
+++ js/gameover.js
@@ -3,19 +3,22 @@ var gameoverState = {
 		drawPatternBG("#222222", "#444444");
 		resetNav();
 		music.stop();
-		var gameoverLabel = game.add.text(80, 80, getText("Gameover", 0),
-			{font: '50px Arial', fill:'#ffffff'});
-		var finalScoreLabel = game.add.text(80, game.world.height - 240, getText("Gameover", 3) + curScore,
-			{font: '25px Arial', fill:'#ffffff'});
-		var finalLevelLabel = game.add.text(80, game.world.height - 200, getText("Gameover", 4) + level,
-			{font: '25px Arial', fill:'#ffffff'});
-		var finalLinesLabel = game.add.text(80, game.world.height - 160, getText("Gameover", 5) + lineCount,
-			{font: '25px Arial', fill:'#ffffff'});
-		var goToMainMenuLabel = game.add.text(80, game.world.height - 320, getText("Gameover", 1),
-			{font: '25px Arial', fill:'#ffffff'});
+		var gameoverLabel = game.add.text(80, 80, getText("Gameover", 0), getStyle("title"));
+		var finalLevelLabel = game.add.text(80, game.world.height - 280, getText("Gameover", 3) + curScore, getStyle("text_big"));
+		var finalLevelLabel = game.add.text(80, game.world.height - 250, getText("Gameover", 4) + level, getStyle("text_big"));
+		var finalLinesLabel = game.add.text(80, game.world.height - 220, getText("Gameover", 5) + lineCount, getStyle("text_big"));
 
-		var enterKey = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
-		enterKey.onDown.addOnce(this.start, this);
+		var buttonStyle = getStyle("button_regular");
+
+		btnMainMenu = game.add.button(game.world.width / 2, (game.world.height / 2) + 100, 'big_button', function(){show('menu')}, this, 1, 2, 0);
+		btnMainMenu.anchor.setTo(0.5, 0.5);
+		lblMainMenu = game.add.text(game.world.width / 2, (game.world.height / 2) + 100, getText("Gameover", 1), buttonStyle);
+		lblMainMenu.anchor.setTo(0.5, 0.5);
+
+		btnPlayAgain = game.add.button(game.world.width / 2, (game.world.height / 2) + 160, 'big_button', function(){show('singlePlayerPrep')}, this, 1, 2, 0);
+		btnPlayAgain.anchor.setTo(0.5, 0.5);
+		lblPlayAgain = game.add.text(game.world.width / 2, (game.world.height / 2) + 160, getText("Gameover", 2), buttonStyle);
+		lblPlayAgain.anchor.setTo(0.5, 0.5);
 
 		if(submitScore(curScore) == 0){
 			showNewHighscore();
@@ -27,7 +30,6 @@ var gameoverState = {
 }
 
 function showNewHighscore(){
-	var finalLinesLabel = game.add.text(80, game.world.height - 280, getText("Gameover", 6),
-			{font: '25px Arial', fill:'#cc5555'});
+	var finalLinesLabel = game.add.text(80, game.world.height - 320, getText("Gameover", 6), getStyle("text_warning_big"));
 	name = prompt(getText("Gameover", 7), "");if (name) {updatePlayerName(name);updateLeaderboard();}
 }
